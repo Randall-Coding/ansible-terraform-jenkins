577b82e61a3a4e3b3f80bc15b0f1f0049ce5a22a
Randall Coding <randallcoding@protonmail.com>
2023-10-06 15:44:14 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,306 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+# Get Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+locals {
+  team        = "api_mgmt_dev"
+  application = "corp_api"
+  serve_name  = "ec2-${var.environment}-api-${var.variables_sub_az}"
+  az_names    = data.aws_availability_zones.available.names
+}
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
+
+resource "aws_key_pair" "developer" {
+  key_name   = "developer-${var.environment}"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
+
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = var.vpc_name
+    Environment = var.environment
+    Terraform   = "true"
+    Region      = data.aws_region.current.name
+  }
+
+  enable_dns_hostnames = true
+}
+
+#Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each   = var.public_subnets
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone = try(
+    local.az_names[each.value],
+    local.az_names[0],
+  )
+  map_public_ip_on_launch = true
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+#Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each   = var.private_subnets
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = try(
+    local.az_names[each.value],
+    local.az_names[0]
+  )
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+#Create EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+  }
+}
+
+#Create route tables for public and private subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_public_rtb"
+    Terraform = "true"
+  }
+}
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    # gateway_id = aws_internet_gateway.internet_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
+}
+#Create route table associations
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name  = local.serve_name
+    Owner = local.team
+    App   = local.application
+  }
+  key_name               = aws_key_pair.developer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_web.id]
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> aws_hosts"
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "ingress-443" {
+  name = "web_server_inbound"
+
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_443_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
+
+resource "aws_security_group" "main" {
+  name = "main-global"
+
+  description = "AllowDoes nothing"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "main"
+    Purpose = "Does nothing"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+  bucket = "my-new-tf-test-bucket-${random_id.randomness.hex}"
+  tags = {
+    Name    = "My S3 Bucket"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {
+  bucket = aws_s3_bucket.my-new-S3-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my-new-S3-bucket-acl" {
+  bucket     = aws_s3_bucket.my-new-S3-bucket.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.my_new_bucket_acl]
+}
+
+resource "random_id" "randomness" {
+  byte_length = 16
+}
+
+resource "aws_subnet" "list_subnet" {
+  for_each          = var.ip
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = each.value
+  availability_zone = local.az_names[0]
+  tags = {
+    Environment  = var.environment
+    "CIDR block" = each.value
+  }
+}
+
+output "web_server-ip" {
+  value = aws_instance.web_server.public_ip
+}
---
+++ terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">=1.5.7" #originally made with 1.5.7
+  required_providers {
+    tls = "3.1.0"
+  }
+  # backend "local" {
+  #   path = "test/terraform.tfstate"
+  # }
+
+  # backend "s3" {
+  #   bucket = "my-tf-state-rcoding"
+  #   key = "prod/myapp.tfstate"
+  #   region = "us-east-1"
+
+  #   encrypt = true
+  #   dynamodb_table = "terraform-locks"
+  # }
+
+  # cloud {
+  #   organization = "randallcoding"
+
+  #   workspaces {
+  #     name = "example-workspace"
+  #   }
+  # }
+
+  # backend "remote" {
+  #   hostname = "app.terraform.io"
+  #   organization = "randallcoding"
+
+  #   workspaces {
+  #     name = "example-workspace"
+  #   }
+  # }
+}
