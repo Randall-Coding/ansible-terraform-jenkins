ef9f9e021e3b583b07d541041f3f2652047a14b9
Randall Coding <randallcoding@protonmail.com>
2023-10-11 19:20:28 +0700

    Removes local_file resource for private key

--- main.tf
+++ main.tf
@@ -32,11 +32,6 @@ resource "tls_private_key" "generated" {
   algorithm = "RSA"
 }
 
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.generated.private_key_pem
-  filename = var.my_aws_pem  
-}
-
 resource "aws_key_pair" "developer" {
   key_name   = "developer-${var.environment}"
   public_key = tls_private_key.generated.public_key_openssh
